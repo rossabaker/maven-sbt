3b09ee2e31d035e2502f8c06870dc463a43efd88
Coda Hale <coda.hale@gmail.com>
2010-12-17 17:32:39 -0800

    Don't need all that crap, either.

--- project/build/MavenSbt.scala
+++ project/build/MavenSbt.scala
@@ -1,11 +1,5 @@
 import sbt._
 
 class MavenSbt(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
-//  val maven = "org.apache.maven" % "apache-maven" % "3.0.1"
-//  val mavenCore = "org.apache.maven" % "maven-core" % "3.0.1" withSources()
-//  val mavenModel = "org.apache.maven" % "maven-model" % "3.0.1" withSources()
-//  val mavenModelBuilder = "org.apache.maven" % "maven-model-builder" % "3.0.1" withSources()
-//  val mavenEmbedder = "org.apache.maven" % "maven-embedder" % "3.0.1" withSources ()
-
   val mavenAntTasks = "org.apache.maven" % "maven-ant-tasks" % "2.1.1" withSources()
 }
